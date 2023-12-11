16d829ced46b09d927994ea1f5f9676e61cd7d2e
endalk5728 <endalkengida8@gmail.com>
2023-12-11 06:19:04 -0800

    first commit

---
+++ app/issues/page.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+// import {Button } from '@radix-ui/themes';
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <div>
+   {/* <Button> <Link href="/issues/new"> New issue</Link></Button> */}
+   hello
+   </div>
+  )
+}
+
+export default page
